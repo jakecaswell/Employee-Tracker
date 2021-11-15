a0da9a79305da9633ea149751ff775ba575a0398
jcaswell <jakecaswell88@gmail.com>
2021-11-14 22:30:43 -0500

    created tables for our database and created the database itself

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+Create Table department (
+    id Integer AUTO_INCREMENT Primary Key,
+    name Varchar(30)
+);
+
+Create Table role (
+    id Integer AUTO_INCREMENT Primary Key,
+    title Varchar(30) Not Null, 
+    salary Decimal,
+    department_id Integer,
+    Foreign Key (department_id) References department(id)
+);
+
+Create Table employee (
+    id Integer AUTO_INCREMENT Primary Key,
+    first_name Varchar(30) Not Null,
+    last_name Varchar(30) Not Null,
+    role_id Integer, 
+    manager_id Integer,
+    Foreign Key (role_id) References role(id),
+    Foreign Key (manager_id) References employee(id)
+);
