b8d2c4582f6459a3701351e8931ae7fb7e52fd8c
jcaswell <jakecaswell88@gmail.com>
2021-11-16 20:31:04 -0500

    finished functions for adding things into the database

--- index.js
+++ index.js
@@ -67,10 +67,11 @@ const viewRoles = () => {
 
 const viewEmployees = () => {
     console.log('\nYou chose to view all the employees!\n');
-    const sql = `SELECT employee.first_name, employee.last_name, roles.title As title, roles.salary As salary, department_name As department, employee.manager_id As manager 
+    const sql = `SELECT employee.first_name, employee.last_name, roles.title As title, roles.salary As salary, department_name As department, manager.first_name As manager 
                  FROM employeedb.employee
                  Left Join roles On employee.role_id = roles.id
-                 Left Join department On roles.department_id = department.id`;
+                 Left Join department On roles.department_id = department.id
+                 Left Join employee manager On employee.manager_id = manager.id`;
     connection.query(sql, (err, res) => {
         if (err) {
             throw err;
@@ -94,7 +95,7 @@ const addDepartment = () => {
         connection.query(sql, res.name,
             function(err) {
                 if (err) throw err
-                viewDepartments();
+                console.table(res);
                 startApp();
             }
         )
@@ -103,12 +104,131 @@ const addDepartment = () => {
 
 const addRole = () => {
     console.log('\nYou chose to add another role!\n');
+    inquirer.prompt([
+        {
+            name: 'role',
+            type: 'input',
+            message: 'What role are you adding?'
+        }, 
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'What is the salary for this position?'
+        }
+    ]).then(answer => {
+        const sql = `Insert Into roles (roles.title, roles.salary)
+                     Values (?,?)`;
+        const params = [answer.role, answer.salary]
+        connection.query(sql, params, (err, res) => {
+            if (err) throw err;
+            console.table(answer);
+            startApp();
+        } )
+    })
 }
+
+// gets all the roles from the database into an array, allowing you to choose one when creating an employee
+let roleArr = [];
+const chooseRole = () => {
+    const sql = `Select * From roles`;
+    connection.query(sql, (err, res) => {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            roleArr.push(res[i].title)
+        }
+    })
+    return roleArr;
+}
+
+// gets all the employees into an array, allowing you to choose one as manager when creating employee
+let employeeArr = [];
+const chooseManager = () => {
+    const sql = `Select first_name, last_name From employee`;
+    connection.query(sql, (err, res) => {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            employeeArr.push(res[i].first_name);
+        }
+    })
+    return employeeArr;
+}
+
 
 const addEmployee = () => {
     console.log('\nYou chose to add a new employee!\n');
+    inquirer.prompt([
+        {
+            name: 'firstName',
+            type: 'input',
+            message: 'What is your first name?'
+        },
+        {
+            name: 'lastName',
+            type: 'input',
+            message: 'What is your last name?'
+        },
+        {
+            name: 'role',
+            type: 'list',
+            choices: chooseRole(),
+            message: 'What is your role here?'
+        },
+        {
+            name: 'manager',
+            type: 'list',
+            message: 'Who is your manager?',
+            choices: chooseManager()
+        }
+    ]).then(answer => {
+        const roleId = chooseRole().indexOf(answer.role) + 1
+        const managerId = chooseManager().indexOf(answer.manager) + 1
+        const sql = `Insert Into employee (employee.first_name, employee.last_name, employee.role_id, employee.manager_id)
+                     Values (?,?,?,?)`;
+        const params = [answer.firstName, answer.lastName, roleId, managerId];
+        connection.query(sql, params, (err, res) => {
+            if (err) throw err;
+            console.table(answer);
+            startApp();
+        })
+    })
 }
 
 const updateRole = () => {
     console.log("\nYou chose to update an employee's role!\n");
+    const sql = `Select employee.first_name, roles.title From employee
+Left Join roles On employee.role_id = roles.id`;
+    connection.query(sql, (err, res) => {
+        if (err) throw err;
+        console.log(res);
+        inquirer.prompt([
+            {
+                name: 'employee',
+                type: 'list',
+                message: 'Who would you like to update?',
+                choices: function() {
+                    let updatedEmployee = [];
+                    for (var i = 0; i < res.length; i++) {
+                        updatedEmployee.push(res[i].first_name)
+                    }
+                    return updatedEmployee;
+                }
+            },
+            {
+                name: 'newrole',
+                type: 'list',
+                message: 'What role would you like to choose?',
+                choices: chooseRole()
+            }
+        ]).then(answer => {
+            const roleId = chooseRole().indexOf(answer.newrole) + 1;
+            const sql = `Update employee Set role_id = ?
+            Where first_name =?`;
+            const params = [roleId, answer.employee]
+            connection.query(sql, params, (err, res) => {
+                if (err) throw err;
+                console.table(answer);
+                startApp();
+            })
+        })
+    })
 }
